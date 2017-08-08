3dab2f9ff82d235c0e49ac7db368c9e0a2c0d4fb
khkim <moon6pence@gmail.com>
2017-08-08 22:57:15 +0200

    Refresh screen

--- kilo.c
+++ kilo.c
@@ -19,6 +19,9 @@ struct termios orig_termios;
 
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -61,6 +64,25 @@ char editorReadKey()
 	return c;
 }
 
+/* output */
+
+void editorDrawRows()
+{
+	for (int y = 0; y < 24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen()
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /* input */
 
 void editorProcessKeypress()
@@ -69,6 +91,9 @@ void editorProcessKeypress()
 
 	switch (c) {
 	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+
 		exit(0);
 		break;
 	}
@@ -81,6 +106,7 @@ int main()
 	enableRawMode();
 
 	while (1) {
+		editorRefreshScreen();
 		editorProcessKeypress();
 	}
 
