91ee9dd791274709fadf773b50ca5a2818462854
khkim <moon6pence@gmail.com>
2017-08-08 23:26:03 +0200

    Step 35. tilde on last line

--- kilo.c
+++ kilo.c
@@ -112,7 +112,11 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows()
 {
 	for (int y = 0; y < E.screenrows; y++) {
-		write(STDOUT_FILENO, "~\r\n", 3);
+		write(STDOUT_FILENO, "~", 1);
+
+		if (y < E.screenrows -1) {
+			write(STDOUT_FILENO, "\r\n", 2);
+		}
 	}
 }
 
